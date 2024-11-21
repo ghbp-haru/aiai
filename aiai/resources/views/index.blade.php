1f22f49cf22a3b0e14ac59240bbda89a76d320a4
shibata <0426ghbp@gmail.com>
2024-11-21 03:10:26 +0000

    ログインページ作成

---
+++ aiai/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>相積み運送アプリ　アイアイログイン</title>
+    </head>
+    <body>
+        <h1>相積みアイアイ</h1>
+        <form action="./login" method="post">
+            ユーザーID：<input name="emali"><br>
+            パスワード：<input name="password" type="password"><br>
+            <button>ログイン</button>
+            <button>新規登録</button>
+        </form>
+    </body>
+</html>
